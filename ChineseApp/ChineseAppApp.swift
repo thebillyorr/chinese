d052898287c8d051b9bdfbe2835b29401fe2f96b
Billy Orr <thebillyorr@gmail.com>
2025-11-07 15:04:21 -0800

    Initial Commit

---
+++ ChineseApp/ChineseAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChineseAppApp.swift
+//  ChineseApp
+//
+//  Created by Billy Orr on 2025-11-07.
+//
+
+import SwiftUI
+
+@main
+struct ChineseAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChineseApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChineseApp
+//
+//  Created by Billy Orr on 2025-11-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
